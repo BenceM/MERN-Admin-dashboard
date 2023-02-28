21dce3196737f04a42e0cd2eb37c032dc993adb3
Bence <m.bence211@gmail.com>
2023-02-28 21:00:02 +0100

    init server setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import managementRoutes from "./routes/management.js";
+import generalRoutes from "./routes/general.js";
+import salesRoutes from "./routes/sales.js";
+
+// config
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(cors());
+
+//routing
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
